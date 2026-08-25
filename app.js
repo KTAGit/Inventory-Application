7601ab3a9f219595224ad827b776c536bf9adbb9
KTAGit <121195747+KTAGit@users.noreply.github.com>
2026-08-24 22:17:06 -0400

    Setup database and server connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv"
 import router from "./routes/pageRoute.js"
+import { pool } from "./model/db.js";
 
 dotenv.config()
 const app = express()
@@ -19,10 +20,17 @@ app.use("/", router)
 
 const PORT = process.env.PORT
 
+async function startServer() {
+    try {
+        await pool.query("SELECT 1")
+        console.log("Database connected")
 
-app.listen(PORT, (err) => {
-    if (err) {
-        console.log("Error: ", err)
+        app.listen(PORT, async () => {
+            console.log("Listening on PORT ", PORT)
+        })
+    } catch (err) {
+        console.log("Database faild to connect", err.message)
     }
-    console.log("Listening on PORT ", PORT)
-})
+}
+
+startServer()
